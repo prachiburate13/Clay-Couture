19ffeee48ea3bb64ced81c26c830912b0550d754
pburate <prachiburate123@gmail.com>
2025-01-09 16:08:10 +0530

    Prachi(Home Page design)

---
+++ app/Views/home/home.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clay Couture</title>
+        <link rel="stylesheet" href="<?php echo base_url('/public/css/home.css'); ?>">
+       
+</head>
+<body>
+    <!-- Section - 1 -->
+    <section class="even"> 
+    <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid d-flex justify-content-between">
+        <a href="" class="navbar-brand text-white brand-name" style="width:200px;">Clay Couture</a>  
+        <div class="d-flex">
+            <a href="" class="navbar-brand text-white">Home</a>
+            <a href="" class="navbar-brand text-white">About</a>
+            <a href="" class="navbar-brand text-white">Shop</a>
+            <a href="" class="navbar-brand text-white">Contact</a>
+        </div>
+        <a href="" class="navbar-brand text-white">
+            <i class="fa fa-solid fa-cart-shopping"></i>
+        </a>
+    </div>
+</nav>
+
+<div class="Row" >
+
+<div class="col" style="margin-top:80px;">
+    <div><h6 class="text-white">Welcome to Clay Couture</h6></div>
+    <div><h1 class="text-white" style="font-size:70px; font-family: serif;">Elevate Your <br>Space with <br>Ceramic Elegance.</h1>
+<h3 class="text-white">Starting from just Rs.200/-</h3>
+<br><br>
+<button class="btn btn-outline-light" style="font-size:20px; padding-left:30px; padding-right:30px;">Shop Now</button>
+
+</div>
+
+</div>
+<div class="col" style="position: relative; width: 500px; height: 550px;">
+    <img src="<?php echo base_url("/public/images/ceramic-pots.jpg");?>" style="margin-top:50px;margin-left:50px; width:500px; height:650px;" alt="">
+    <img 
+        src="<?php echo base_url('/public/images/ceramic-pots2.jpg'); ?>" 
+        style="position: absolute; width:300px; height:350px; top: 250px; left: calc(50px - 100px); margin-right: 450px;"
+
+        alt="Overlay Image"
+    >
+</div>
+
+</div>
+</section>
+
+<!-- Section - 2 -->
+<section class="section_2">
+
+<div><h4 style="color:#025048;  font-size:20px; font-family:monospace; margin-top:100px;">About Clay Couture</h4>
+<h1 style="color:#025048;  font-family: serif; font-size:60px;">The versatility of ceramics is what makes them truly remarkable, with their presence in various forms such as stoneware and porcelain.</h1>
+</div>
+
+<div class="row">
+<div class="col">
+<img src="<?php echo base_url("/public/images/about-01.jpg");?>" style="margin-top:50px;margin-left:50px; width:500px; height:650px;" alt="">
+</div>
+<div class="col" style="margin-left:50px;">
+<img src="<?php echo base_url("/public/images/about-02.jpg");?>" style="margin-top:120px; width:600px; height:350px;" alt="">
+<h2 style="color:#025048;  font-family: serif; font-size:25px; margin-top:30px;">Welcome to Ceramic Shop, where passion meets craftsmanship to bring you a world of timeless beauty and creativity.</h2>
+<h4 style="color:#025048;  font-family: monospace; font-size:15px; margin-top:20px;">Our journey is steeped in the art of ceramics, where each piece tells a unique story. Get to know the heart and soul behind our store.</h4>
+<button class="btn btn-outline-light" style="font-size:20px; font-family:monospace; padding-left:30px; padding-right:30px; color:#025048; border-color:#025048;">Read More</button>
+</div>
+
+</div>
+</section>
+<section class="section-3">
+ <div class="row">
+<div class="col">
+<h2 style="color:#025048;  font-family: monospace; font-size:20px; margin-top:30px;">THE BEST OR CERMAICS</h2>
+<h1 style="color:#585c5b;  font-family: serif; font-size:50px; margin-top:30px;">Our Products Category​</h1>
+    <h3 style="color:#025048; font-family: monospace; font-size:15px; margin: 0; text-align: left;">
+        Explore our exquisite collection of ceramic treasures that elevate your spaces and celebrate the artistry of craftsmanship. From dinnerware to decor, each piece in our product range is a testament to quality, style, and the timeless beauty of ceramics.
+    </h3>
+</div>
+<div class="col">
+
+</div>
+ </div>
+</section>
+</body>
+</html>
